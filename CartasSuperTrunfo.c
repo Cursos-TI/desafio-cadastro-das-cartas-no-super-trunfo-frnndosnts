3a41edd3ed70bb13e3e689b79d9f132d0d111c02
frnndosnts <luiissfernando11@gmail.com>
2025-03-24 00:51:02 +0000

    Conclui nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,10 @@
 int main(){
 
     char estado [50], nome_da_cidade [50], codigo_da_carta [50];
-    int populacao, pontos_turisticos;
-    float area, pib, densidade_populacional, pib_per_capita;
+    int pontos_turisticos;
+    unsigned long int populacao;
+    float area, pib, densidade_populacional, pib_per_capita, super_poder;
+    int ResultadoA , ResultadoB, ResultadoC, ResultadoD, ResultadoE , ResultadoF, ResultadoG;
 
 
   // Utilizando do scanf para entrada de dados externos ao código, preenchendo informações das cartas.
@@ -20,7 +22,7 @@ int main(){
     printf("Digite o código da carta: \n");
     scanf("%s", &codigo_da_carta);
     printf("Digite o número da população: \n");
-    scanf("%d", &populacao);
+    scanf("%u", &populacao);
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos);
     printf("Digite a área: \n");
@@ -31,6 +33,7 @@ int main(){
   //calcular densidade populacional e pib per capita.
     densidade_populacional = (float)(populacao / area);
     pib_per_capita = (float)(pib / populacao);
+    super_poder = (float)(populacao + area + pib + pontos_turisticos + pib_per_capita + (1 / densidade_populacional));
 
   // Imprimir as informações com o printf.
 
@@ -38,12 +41,13 @@ int main(){
     printf("Estado: %s\n", estado);
     printf("Nome da Cidade: %s\n", nome_da_cidade);
     printf("Código da Carta: %s\n", codigo_da_carta);
-    printf( "População: %d\n", populacao);
+    printf( "População: %u\n", populacao);
     printf("Pontos Turísticos: %d\n", pontos_turisticos);
     printf("Área: %.2f km²\n ", area);
     printf("Número PIB: %.2f bilhões de reais\n ", pib);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional);
     printf("PIB Per Capita: %.2f reais\n", pib_per_capita);
+    printf("Super poder: %.2f\n", super_poder);
 
   // Preenchendo os dados da próxima carta.
 
@@ -54,7 +58,7 @@ int main(){
     printf("Digite o código da carta: \n");
     scanf("%s", &codigo_da_carta);
     printf("Digite o número da população: \n");
-    scanf("%d", &populacao);
+    scanf("%u", &populacao);
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos);
     printf("Digite a área: \n");
@@ -64,17 +68,38 @@ int main(){
 
     densidade_populacional = (float)(populacao / area);
     pib_per_capita = (float)(pib / populacao);
+    super_poder = (float)(populacao + area + pib + pontos_turisticos + pib_per_capita + (1 / densidade_populacional));
+
 
     printf("Carta 2:\n");
     printf("Estado: %s\n", estado);
     printf("Nome da Cidade: %s\n", nome_da_cidade);
     printf("Código da Carta: %s\n", codigo_da_carta);
-    printf( "População: %d\n", populacao);
+    printf( "População: %u\n", populacao);
     printf("Pontos Turísticos: %d\n", pontos_turisticos);
     printf("Área: %.2f km²\n", area);
     printf("Número PIB: %.2f bilhões de reais\n", pib);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional);
     printf("PIB Per Capita: %.2f reais\n", pib_per_capita);
+    printf("Super poder: %.2f\n", super_poder);
+
+  // Compare as duas cartas.
+    ResultadoA = populacao > populacao;
+    ResultadoB = pontos_turisticos > pontos_turisticos;
+    ResultadoC = area > area;
+    ResultadoD =  pib > pib;
+    ResultadoE = densidade_populacional < densidade_populacional;
+    ResultadoF = pib_per_capita > pib_per_capita;
+    ResultadoG = super_poder > super_poder;
+       
+    printf( "Comparação de Cartas:\n");
+    printf("População: Carta %d venceu\n", ResultadoA);
+    printf("Área: Carta %d venceu\n", ResultadoC);
+    printf("PIB: Carta %d venceu\n", ResultadoD);
+    printf("Pontos Turísticos: Carta %d venceu\n", ResultadoB);
+    printf("Densidade populacional: Carta %d venceu\n", ResultadoE);
+    printf("PIB Per Capita: Carta %d venceu\n", ResultadoF);
+    printf("Super Poder: Carta %d venceu\n", ResultadoG);
 
     return 0;
 }
